9356c13d1137be710ec691e571f470c9d70fb3b9
laura19Caceresj <lauracaceresj19@gmail.com>
2025-12-11 17:45:22 -0500

    feature:create class heroes.data
    create  class heroes.data

---
+++ src/data/heroes.data.ts
@@ -0,0 +1,48 @@
+interface Hero {
+id: number;
+name:string;
+owner: Owner;// Interfaz definoda abajo 
+}
+
+///type Owner = 'DC' | 'Marvel';// interfaz que se definio tipos de datos para vaalores especificos
+
+//enumeraciones
+enum Owner{
+    DC='DC',
+    Marvel='Marvel'
+}
+
+const heroes: Hero [] = [
+  {
+    id: 1,
+    name: 'Batman',
+    owner:Owner.DC,
+  },
+  {
+    id: 2,
+    name: 'Spiderman',
+    owner: Owner.Marvel ,
+  },
+  {
+    id: 3,
+    name: 'Superman',
+    owner: Owner.DC,
+  },
+  {
+    id: 4,
+    name: 'Flash',
+    owner: Owner.DC
+    ,
+  },
+  {
+    id: 5,
+    name: 'Wolverine',
+    owner: Owner.Marvel,
+  },
+
+  {
+    id:6,
+    name:"green lantern",
+    owner:Owner.DC
+  }
+];
