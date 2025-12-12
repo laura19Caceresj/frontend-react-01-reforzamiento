df01418058a87668623fd295ee8fe0490fe2177a
laura19Caceresj <lauracaceresj19@gmail.com>
2025-12-12 16:07:43 -0500

    feature:create class promises
    create class promises

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,9 @@ import './style.css'
 //import './bases/06-obj-destructuring'
 //import './bases/07-array-destructuring'
 //import './data/heroes.data.ts'
-import './bases/08-imp-exp.ts'
+//import './bases/08-imp-exp.ts'
 
+import './bases/09-promises.ts'
 //import {getHeroesByOwner} from './bases/08-imp-exp.ts'
 //import { Owner } from './data/heroes.data.ts'
 
